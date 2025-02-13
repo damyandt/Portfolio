3b8e855b8adf7b98c7a555380c6540d0f1b85de0
Damian Todorov <damqn_dimitrov1111@icloud.com>
2025-02-13 21:13:12 +0200

    update
    add sleep with me

--- vite-project/src/components/AboutMe.jsx
+++ vite-project/src/components/AboutMe.jsx
@@ -8,9 +8,7 @@ export const AboutMe = () => {
             <div className="infoMe">
                 <h4>ABOUT ME</h4>
                 <h3>Front-end Developer based in Sofia, Bulgaria 📍</h3>
-                <p>As a Junior Front-End Developer, I possess an impressive arsenal of skills in HTML, CSS, JavaScript, React and Angular. I excel in designing and maintaining responsive websites that offer a smooth user experience. My expertise lies in crafting dynamic, engaging interfaces through writing clean and optimized code and utilizing cutting-edge development tools and techniques. I am also a team player who thrives in collaborating with cross-functional teams to produce outstanding web applications.</p>
-                {/* <p>Hey, my name is Damyan, and I'm a Frontend Developer. My passion is to create and develop a clean UI/UX for my users.</p>
-                <p>My main stack currently is React/Next.js and Angular in combination with HTML, CSS and TypeScript.</p> */}
+                <p>As a Junior Front-End Developer, I possess an impressive arsenal of skills in JavaScript, React, Angular, HTML and CSS. I excel in designing and maintaining responsive websites that offer a smooth user experience. My expertise lies in crafting dynamic, engaging interfaces through writing clean and optimized code and utilizing cutting-edge development tools and techniques. I am also a team player who thrives in collaborating with cross-functional teams to produce outstanding web applications.</p>
             </div>
         </section>
 
